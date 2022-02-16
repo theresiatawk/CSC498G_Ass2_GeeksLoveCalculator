8453eaa296d6d0186e1b988c996fe51d73feb0d7
Theresia Tawk <theresiatawk@hotmail.com>
2022-02-16 23:59:08 +0200

    Finalizing Table

--- app/src/main/java/com/lau/ass2geekslovecalculator/MainActivity.java
+++ app/src/main/java/com/lau/ass2geekslovecalculator/MainActivity.java
@@ -2,14 +2,19 @@ package com.lau.ass2geekslovecalculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.Image;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Adapter;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Spinner;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -18,10 +23,13 @@ import java.util.Random;
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
     int lower_bound = 0;
     int upper_bound = 100;
+    int random_number;
+    String name, language_selected;
     TextView view_result;
-    Button calculate_result;
-    String language_selected;
+    Button calculate_result, reset;
     ImageView language_icon;
+    EditText full_name;
+    TableLayout result_table;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +50,15 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         view_result = (TextView) findViewById(R.id.textResult);
         calculate_result = (Button) findViewById(R.id.calculateButton);
         language_icon = (ImageView) findViewById(R.id.languageIcon);
+        full_name = (EditText) findViewById(R.id.personName);
+        result_table = (TableLayout) findViewById(R.id.resultTable);
+        reset = (Button) findViewById(R.id.resetButton);
+
+        full_name.setText("");
+        reset.setVisibility(View.GONE);
+        calculate_result.setText("Calculate Love");
+        result_table.setVisibility(View.GONE);
+
     }
 
     @Override
@@ -49,32 +66,84 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         String text = parent.getItemAtPosition(position).toString();
         language_selected = (String) text;
     }
-
     @Override
     public void onNothingSelected(AdapterView<?> adapterView) {
         Toast.makeText(this, "Enter Something",Toast.LENGTH_LONG).show();
     }
     public void calculate(View view){
-        Random random = new Random();
-        int random_number = random.nextInt(upper_bound-lower_bound) + lower_bound;
-        String result = "Your love to " +language_selected+ " language is " +random_number+ " %";
-        view_result.setText(result);
-        if (language_selected.equalsIgnoreCase( "Python"))
-            language_icon.setImageResource(R.drawable.python);
-        else if (language_selected.equalsIgnoreCase( "Java"))
-            language_icon.setImageResource(R.drawable.java);
-        else if (language_selected.equalsIgnoreCase( "Kotlin"))
-            language_icon.setImageResource(R.drawable.kotlin);
-        else if (language_selected.equalsIgnoreCase( "C"))
-            language_icon.setImageResource(R.drawable.c);
-        else if (language_selected.equalsIgnoreCase( "C++"))
-            language_icon.setImageResource(R.drawable.cplusplus);
-        else if (language_selected.equalsIgnoreCase( "PHP"))
-            language_icon.setImageResource(R.drawable.php);
-        else if (language_selected.equalsIgnoreCase( "HTML"))
-            language_icon.setImageResource(R.drawable.html);
-        else{
-            language_icon.setImageResource(R.drawable.html);
+        name = (String) full_name.getText().toString();
+
+        if (name.length() == 0) {
+            String message = "Error: You should fill your name";
+            Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
         }
+        else{
+            // Generate a random number from 0 to 100
+            Random random = new Random();
+            random_number = random.nextInt(upper_bound-lower_bound) + lower_bound;
+
+            //Displaying the text
+            String result = (String) name + " your love to " +language_selected+ " language is " +random_number+ " %";
+            view_result.setText(result);
+
+            //Animating
+            language_icon.setTranslationY(-1500);
+
+            if (language_selected.equalsIgnoreCase("Python"))
+                language_icon.setImageResource(R.drawable.python);
+            else if (language_selected.equalsIgnoreCase( "Java"))
+                language_icon.setImageResource(R.drawable.java);
+            else if (language_selected.equalsIgnoreCase( "Kotlin"))
+                language_icon.setImageResource(R.drawable.kotlin);
+            else if (language_selected.equalsIgnoreCase( "C"))
+                language_icon.setImageResource(R.drawable.c);
+            else if (language_selected.equalsIgnoreCase( "C++"))
+                language_icon.setImageResource(R.drawable.cplusplus);
+            else if (language_selected.equalsIgnoreCase( "PHP"))
+                language_icon.setImageResource(R.drawable.php);
+            else if (language_selected.equalsIgnoreCase( "HTML"))
+                language_icon.setImageResource(R.drawable.html);
+            language_icon.animate().translationYBy(1500).rotation(3600).setDuration(1200);
+            calculate_result.setText("Play Again");
+            reset.setVisibility(View.VISIBLE);
+
+            displayRecord();
+        }
+    }
+    public void displayRecord(){
+        result_table.setVisibility(View.VISIBLE);
+        TableRow row1 = new TableRow(this);
+        TextView user = new TextView(this);
+        TextView language = new TextView(this);
+        TextView score = new TextView(this);
+
+
+        user.setText(name);
+        language.setText(language_selected);
+        score.setText(random_number + "%");
+
+        user.setGravity(Gravity.CENTER);
+        language.setGravity(Gravity.CENTER);
+        score.setGravity(Gravity.CENTER);
+
+        user.setTextSize(20);
+        language.setTextSize(20);
+        score.setTextSize(20);
+
+        row1.addView(user);
+        row1.addView(language);
+        row1.addView(score);
+
+        result_table.addView(row1);
+
+    }
+    public void reset (View v){
+        full_name.setText("");
+        view_result.setText("");
+        calculate_result.setText("Calculate Love");
+        reset.setVisibility(View.GONE);
+        language_icon.setImageDrawable(null);
+        result_table.setVisibility(View.GONE);
+
     }
 }
